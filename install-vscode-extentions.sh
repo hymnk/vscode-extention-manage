c2e96035c762ed4e5b1979019ade18038c1b0e4c
hymnk <6967985+hymnk@users.noreply.github.com>
2022-03-09 01:06:03 +0900

    first commit

---
+++ install-vscode-extentions.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# execute command
+# -------------------
+# curl -s https://raw.githubusercontent.com/karaage0703/vscode-dotfiles/master/install-vscode-extensions.sh | /bin/bash
+
+# Visual Studio Code :: Package list
+pkglist=(
+ms-vscode.cpptools
+ms-python.python
+kelvin.vscode-sshfs
+letmaik.git-tree-compare
+donjayamanne.githistory
+eamodio.gitlens
+coenraads.bracket-pair-colorizer-2
+pkief.material-icon-theme
+yzane.markdown-pdf
+github.github-vscode-theme
+oderwat.indent-rainbow
+#streetsidesoftware.code-spell-checker
+christian-kohler.path-intellisense
+ms-vscode-remote.vscode-remote-extensionpack
+mhutchie.git-graph
+Gruntfuggly.todo-tree
+hediet.vscode-drawio
+github.vscode-pull-request-github
+marp-team.marp-vscode
+shd101wyy.markdown-preview-enhanced
+shuworks.vscode-table-formatter
+esbenp.prettier-vscode
+davidanson.vscode-markdownlint
+njpwerner.autodocstring
+ms-python.vscode-pylance
+# ms-vscode-remote.remote-ssh
+github.github-vscode-theme
+)
+
+for i in ${pkglist[@]}; do
+  code --install-extension $i
+done
