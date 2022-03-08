dfab5fa8ddc9990ffa831d267cb7e7b4db8c958a
hymnk <6967985+hymnk@users.noreply.github.com>
2022-03-09 01:08:28 +0900

    excecute commandの文言変更

--- install-vscode-extentions.sh
+++ install-vscode-extentions.sh
@@ -2,7 +2,7 @@
 
 # execute command
 # -------------------
-# curl -s https://raw.githubusercontent.com/karaage0703/vscode-dotfiles/master/install-vscode-extensions.sh | /bin/bash
+# curl -s (path to shell URL) | /bin/bash
 
 # Visual Studio Code :: Package list
 pkglist=(
